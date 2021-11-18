a0c1335b6c5444a70d5f23c38e8cebc0015a604c
z-takacs <L00169723@student.lyit.ie>
2021-11-18 21:50:08 +0000

    File created.

---
+++ SSH.py
@@ -0,0 +1,22 @@
+"""
+
+#
+# File      :SSH.py
+# Created   : 18/11/2021 21:39
+# Author    : Zoltan Takacs
+# Version   : v1.0.0
+# Licensing : (C) 2021 Zoltan Takacs, LYIT
+#             Available under GNU Public License (GPL)
+# Desription: Connecting from host PC to OOPR VM in VMware WS with SSH through a Python script.
+#
+"""
+if __name__ == '__main__':
+
+'''
+  Main method of application:                                                                                                                                                                                                            
+
+  Parameters: None
+
+  Returns: The logged in user on OOPR virtual machine.
+
+'''
