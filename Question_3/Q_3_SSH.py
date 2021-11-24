1594236c1cfcef8eeb564aaad511ace278997793
z-takacs <L00169723@student.lyit.ie>
2021-11-24 07:46:42 +0000

    2nd attempt

--- Question_3/Q_3_SSH.py
+++ Question_3/Q_3_SSH.py
@@ -7,7 +7,7 @@
 # Version   : v1.0.0
 # Licensing : (C) 2021 Zoltan Takacs, LYIT
 #             Available under GNU Public License (GPL)
-# Desription:
+# Desription: Connecting from host PC to OOPR VM in VMware WS with SSH through a Python script.
 #
 """
 if __name__ == '__main__':
@@ -17,6 +17,34 @@ if __name__ == '__main__':
 
   Parameters: None
 
-  Returns: None
+  Returns: Returns result of connection test.
 
 '''
+import paramiko
+import time
+import re
+
+#Open SSH connection to the device
+def ssh_connection (ip):
+    try:
+        username = "l00169723" #In an automation script read data from file
+        password = "XXXXX" # PW is masked
+
+
+        print("Establishing a connection...")
+        session = SSHClient = paramiko.SSHClient()
+        session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        session.connect(ip.rstrip("\n"), username=username, password=password)
+        connection = session.invoke_shell()
+        connection.send("ls > dir_contents.txt\n") #unix command to list  directory contents and save to file
+        time.sleep(1)
+
+        vm_output = connection.recv(65535)
+        if re.search(b"% Invalid input", vm_output):
+            print("There was an error on vm {}".format(ip))
+        else:
+            print("Commands successfully executed on {}".format(ip))
+        session.close()
+    except paramiko.AuthenticationException:
+        print("Authentication Error")
+ssh_connection("192.168.136.128") # The IP address of VM OOPR.
