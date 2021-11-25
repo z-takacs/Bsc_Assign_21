d004d59a984109cefdc4cc88a23d51ed9a4a9672
z-takacs <L00169723@student.lyit.ie>
2021-11-25 01:04:47 +0000

    Scraping v.1 code + Parsing v.1

--- Question_2/Q_2_WebScraping/Q_2_WebScraping.py
+++ Question_2/Q_2_WebScraping/Q_2_WebScraping.py
@@ -7,7 +7,7 @@
 # Version   : v1.0.0
 # Licensing : (C) 2021 Zoltan Takacs, LYIT
 #             Available under GNU Public License (GPL)
-# Description: Web Scraping the Apache webpage from the OOPR VM.
+# Description: Web Scraping of the Apache2 landing page from the OOPR VM.
 #
 """
 if __name__ == '__main__':
@@ -17,15 +17,23 @@ if __name__ == '__main__':
 
   Parameters: None
 
-  Returns: None
+  Returns: Scrapings of the Apache2 landing page.
 
     '''
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+
 
 url = "http://192.168.136.128/"
 
 result = requests.get(url)
 doc= BeautifulSoup (result.text, "html.parser")
 print (doc.prettify())
+
+print("================================================================")
+print("                        Scraping Ends")
+print("================================================================")
+
+
+print("Q.2.2: The word Apache2 appears {} times in the Apache2 landing page.")
 
