843ab73ceae4eb8263194344de1343b7e7153035
z-takacs <L00169723@student.lyit.ie>
2021-11-30 23:27:52 +0000

    Question 2 Revised.

--- Question_2/Q_2_WebScraping/Q_2_WebScraping.py
+++ Question_2/Q_2_WebScraping/Q_2_WebScraping.py
@@ -16,17 +16,17 @@ if __name__ == '__main__':
 
   Parameters: None
 
-  Returns: Scrapings of the Apache2 landing page.
+  Returns: Scrapings of the Apache2 landing page. Answers for additional questions
 
     '''
 import requests
 from bs4 import BeautifulSoup
-
 url = "http://192.168.136.128/"
 
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "lxml")
 print(doc.prettify())
+
 
 with open("results.text", "w") as f:
     f.write(result.text)
@@ -36,17 +36,23 @@ with open("results.text", "w") as f:
 print(50 * "=")
 print(20*" "+"Questions")
 print(50 * "=")
+print(50 * "=")
 
 file = open("results.text", "r")
 data = file.read()
-print("Q.2.1:")
-################################
+
+print("Q.2.1: What are the headings?")
+print("Answer:")
+print("="*50)
+
+file = open("results.text", "r")
+Apache2 = data.count("Apache2")
+print("Q.2.2:  How many times does the word Apahce2 appear?")
+print("Answer: The word Apache2 appears {} times in the Apache2 landing page.".format(Apache2))
+print("="*50)
+
 file = open("results.text", "r")
 data = file.read()
-Apache2 = data.count("Apache2")
-print("Q.2.2: The word Apache2 appears {} times in the Apache2 landing page.".format(Apache2))
-################################
-file = open("results.text","r")
-data = file.read()
 words = data.split()
-print("Q.2.3: The number of words in this document is:",len(words))
+print("Q.2.3:  How many words this document consist of?")
+print("Answer: The number of words in this document is:", len(words))
